e3303d44df3bda82e776c67fb0c67e09e562c3bb
canndroid <36055946+canndroid@users.noreply.github.com>
2020-12-28 20:38:53 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -117,7 +117,9 @@ if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
 fi
 
 # 2. Select node version
-selectNodeVersion
+# selectNodeVersion
+    NPM_CMD=npm
+    NODE_EXE=node
 
 # 3. Install NPM packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
